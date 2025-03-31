6629e02e5ae54d784f8e9cd7d5982282ef92e041
AlexLowczyk <alex.lowczyk@gmail.com>
2025-03-31 17:51:22 -0400

    added starter stuff

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Title from './title.tsx'
+
+
+function App() {
+
+
+    return (
+        <>
+            <Title />
+        </>
+    )
+}
+
+
+export default App
---
+++ src/title.tsx
@@ -0,0 +1,6 @@
+export function Title(){
+    return(
+        <h1>CS3733 Homework Group 46 Reading and Skiing</h1>
+    )
+}
+export default Title;
