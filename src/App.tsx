c9365e339f54b53d578cc838f3b8182c8f247038
AlexLowczyk <alex.lowczyk@gmail.com>
2025-04-01 16:04:54 -0400

    added Alex Hobby Stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Title from './title.tsx'
 import Intro from './intro.tsx'
 import {Hobby1} from './hobby1.tsx'
+import HobbyTwo from "./hobbyTwo.tsx"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <Title />
             <Intro nameOne="Felicia Moua" nameTwo="Alex Lowczyk"/>
             <Hobby1 />
+            <HobbyTwo></HobbyTwo>
         </div>
     )
 }
